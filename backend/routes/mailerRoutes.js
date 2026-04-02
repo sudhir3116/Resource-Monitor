adec3a0b242e07d515ebad95e04d04272b43005e
Sudhir <sudhir3116@gmail.com>
2026-04-02 16:15:46 +0530

    Cleanup: removed temporary diagnostic scripts and reports

--- backend/routes/mailerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const mailerController = require('../controllers/mailerController')
-const auth = require('../middleware/authMiddleware')
-
-// POST /api/mailer/send-test  (authenticated)
-router.post('/send-test', auth, mailerController.sendTest)
-
-module.exports = router
